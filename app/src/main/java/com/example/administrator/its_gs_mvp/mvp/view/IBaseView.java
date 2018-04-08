9b5c3545bf4aac515c5eee20f513ba3aea1d00e7
Xu01 <123456>
2018-04-08 15:13:56 +0800

    //（国赛题）MVP

---
+++ app/src/main/java/com/example/administrator/its_gs_mvp/mvp/view/IBaseView.java
@@ -0,0 +1,11 @@
+package com.example.administrator.its_gs_mvp.mvp.view;
+
+import android.content.Context;
+
+/**
+ * @Created by xww on 2018/4/8 0008.
+ */
+
+public interface IBaseView {
+    Context getContext();
+}
